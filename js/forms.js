8458cbb41852f0688a68995b4aea641e47577183
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-15 23:00:48 -0300

    created form for subscriptions in subscription.html and modified css files to format the new html file

---
+++ js/forms.js
@@ -0,0 +1,8 @@
+import "functions.js"
+
+function fullName(str) {
+    if(str.lenght <= 6){
+        //modificar el span con el error
+    }
+    
+}
---
+++ js/functions.js
@@ -0,0 +1,5 @@
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i);
+}
+
+
