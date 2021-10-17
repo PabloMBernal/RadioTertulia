ad185faf7c4bfa533b25a887cb057181ecec3484
Pablo Bernal <semperfi1992.pb@gmail.com>
2021-10-17 17:22:10 -0300

    troubleshooting commit

--- js/forms.js
+++ js/forms.js
@@ -1,8 +1,26 @@
-import "functions.js"
-
-function fullName(str) {
-    if(str.lenght <= 6){
-        //modificar el span con el error
-    }
-    
+function isLetter(str) {
+    return str.length === 1 && str.match(/[a-z]/i) ;
 }
+
+function isSpace(str) {
+    return str.length === 1 && str === ' ';    
+}
+
+function chequeo(str) {
+    for(var i= 0; i<str.length;i++){
+        if(!isLetter(str[i])){
+            if(isSpace(str)){
+                continue;
+            }
+            else{
+                console.log("no es letra");
+            }
+        }
+    }    
+}
+
+
+
+
+
+console.log(document.forms)
